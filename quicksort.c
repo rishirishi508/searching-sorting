6fc915974027fb08d6a3931d6fa397ffc8b7b8a5
PonRishi <rishirishi0307@gmail.com>
2025-03-07 20:11:09 +0530

    Update quicksort.c

--- quicksort.c
+++ quicksort.c
@@ -25,9 +25,19 @@ void quickSort(int arr[], int low, int high) {
 }
 
 int main() {
-    int arr[] = {10, 7, 8, 9, 1};
-    int n = sizeof(arr) / sizeof(arr[0]);
+    int n;
+    printf("Enter number of elements: ");
+    scanf("%d", &n);
+    int arr[n];
+
+    printf("Enter %d elements: ", n);
+    for (int i = 0; i < n; i++) {
+        scanf("%d", &arr[i]);
+    }
+
     quickSort(arr, 0, n - 1);
+
+    printf("Sorted array: ");
     for (int i = 0; i < n; i++) {
         printf("%d ", arr[i]);
     }
