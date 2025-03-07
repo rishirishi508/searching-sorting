4eb48a2970c597b71d7969aa50a45bfcc24425f2
PonRishi <rishirishi0307@gmail.com>
2025-03-07 20:07:39 +0530

    Create binarysearch.c

---
+++ binarysearch.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+int binarySearch(int arr[], int left, int right, int key) {
+    while (left <= right) {
+        int mid = left + (right - left) / 2;
+        if (arr[mid] == key) return mid;
+        if (arr[mid] < key) left = mid + 1;
+        else right = mid - 1;
+    }
+    return -1;
+}
+
+int main() {
+    int arr[] = {1, 3, 5, 7, 9};
+    int n = sizeof(arr) / sizeof(arr[0]);
+    int key = 5;
+    int result = binarySearch(arr, 0, n - 1, key);
+    if (result != -1)
+        printf("Found at index %d", result);
+    else
+        printf("Not found");
+}
